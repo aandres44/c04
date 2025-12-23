8f98469dda52ec60f3f2f41464feaca2b31cdaf5
Andres Aguirre <aguirre.alvarez.andres@gmail.com>
2025-12-22 20:38:12 -0600

    Adding constants and fully functional game modes

--- helpers.py
+++ helpers.py
@@ -1,5 +1,11 @@
 import os
 
+ROW_COUNT = 6
+COLUMN_COUNT = 7
+BOARD_SIZE = ROW_COUNT * COLUMN_COUNT
+WINNING_STREAK = 4
+PLAYERS = {0:"X", 1:"O"}
+
 class terminal_colors:
     HEADER = '\033[95m'
     OK_BLUE = '\033[94m'
@@ -11,51 +17,50 @@ class terminal_colors:
     BOLD = '\033[1m'
     UNDERLINE = '\033[4m'
 
+# Print the current board to the terminal
 def draw_board(board: list[str], last_move: int=-1):
-    #board = "|1|2|3|\n|4|5|6|\n|7|8|9|"
     os.system('cls' if os.name == 'nt' else 'clear')
-    for i in range(7):
+    for i in range(COLUMN_COUNT):
         print(f"  {i}", end="")
     print(" ")
     for i, spot in enumerate(board):
         spot = format_player(spot)
-        if (i+1) % 7 == 0: print("|" + spot + "|")
+        if (i+1) % COLUMN_COUNT == 0: print("|" + spot + "|")
         else: print("|" + spot, end="")
     if last_move >= 0: print(f"Last move: {last_move}")
 
+# Check what player has the turn
 def check_side(ply: int) -> str:
-    if ply % 2 == 0: return 'O'
-    else: return 'X'
+    return PLAYERS[ply % 2]
 
 def check_win(board: list[str], pos: int, player:str) -> bool:
     # Horizontal
     # Make a loop in the horizontal sides relative to the position (last move)
     match_count = 0
-    #pos = position[0]
-    low = (pos // 7) * 7
-    high = low + 7
+    low = (pos // COLUMN_COUNT) * COLUMN_COUNT
+    high = low + COLUMN_COUNT
 
     for i in range(low, high):
         if board[i] == player:
             match_count += 1
-            if match_count == 4: return True
+            if match_count == WINNING_STREAK: return True
         else: match_count = 0
-        if match_count + (high - 1 + i) < 4: break
+        if match_count + (high - 1 + i) < WINNING_STREAK: break
 
     # Vertical
     # Make a loop in the vertical sides relative to the position (last move)
     match_count = 0
     i = pos
-    while i >= 7:
-        i -= 7
+    while i >= COLUMN_COUNT:
+        i -= COLUMN_COUNT
 
-    while i < 42:
+    while i < BOARD_SIZE:
         if board[i] == player:
             match_count += 1
-            if match_count == 4: return True
+            if match_count == WINNING_STREAK: return True
         else: match_count = 0
-        # misses optimization that calculates if the remaining spots in the column + match_count < 4
-        i += 7
+        # misses optimization that calculates if the remaining spots in the column + match_count < WINNING_STREAK
+        i += COLUMN_COUNT
 
     # DIAGONALs - we can move around the array using addition and subtraction ex:
     """
@@ -70,62 +75,66 @@ def check_win(board: list[str], pos: int, player:str) -> bool:
     # Diagonal Up Down
     match_count = 0
     i = pos
-    while i % 7 != 0 and i > 7:
+    while i % COLUMN_COUNT != 0 and i > COLUMN_COUNT:
         i = diagonal_up_left(i)
     # Check for short diagonals
-    if (4 <= i <= 6) or i == 21 or i == 28 or i == 35:
-        i = 42
-
-    #print(i)
+    if (4 <= i <= ROW_COUNT) or i == 21 or i == 28 or i == 35:
+        i = BOARD_SIZE
 
     while i < 38:
-        #print(board[i], match_count, i)
         if board[i] == player:
             match_count += 1
-            if match_count == 4: return True
+            if match_count == WINNING_STREAK: return True
         else: match_count = 0
-        # misses optimization that calculates if the remaining spots in the column + match_count < 4
-        if i == 27 or i == 34 or i == 41:
-            i = 42
+        # misses optimization that calculates if the remaining spots in the column + match_count < WINNING_STREAK
+        if i == 27 or i == 34 or i == (BOARD_SIZE - 1 ):
+            i = BOARD_SIZE
         i = diagonal_down_right(i)
 
     # Diagonal Down Up
     match_count = 0
     i = pos
-    while i % 7 != 0 and i < 35:
+    while i % COLUMN_COUNT != 0 and i < 35:
         i = diagonal_down_left(i)
 
     # Check for short diagonals
-    if (39 <= i <= 41) or i == 0 or i == 7 or i == 14:
-        i = -42
+    if (39 <= i <= (BOARD_SIZE - 1 )) or i == 0 or i == COLUMN_COUNT or i == 14:
+        i = - BOARD_SIZE
 
     while i >= 3:
         if board[i] == player:
             match_count += 1
-            if match_count == 4: return True
+            if match_count == WINNING_STREAK: return True
         else: match_count = 0
-        # misses optimization that calculates if the remaining spots in the column + match_count < 4
-        if i == 20 or i == 13 or i == 6:
-            i = -42
+        # misses optimization that calculates if the remaining spots in the column + match_count < WINNING_STREAK
+        if i == 20 or i == 13 or i == ROW_COUNT:
+            i = - BOARD_SIZE
         i = diagonal_up_right(i)
     
     return False
 
+# Get the corresponding column for the given spot
+def get_column(move: int):
+    while move >= COLUMN_COUNT:
+        move -= COLUMN_COUNT
+    return move
+
+
 def diagonal_up_left(position: int) -> int:
-    return position - 8
+    return position - COLUMN_COUNT + 1
 
 def diagonal_down_right(position: int) -> int:
-    return position + 8
+    return position + COLUMN_COUNT + 1
 
 def diagonal_up_right(position: int) -> int:
-    return position - 6
+    return position - ROW_COUNT
 
 def diagonal_down_left(position: int) -> int:
-    return position + 6
+    return position + ROW_COUNT
 
-
+# Give colors and format to the player character
 def format_player(player: str) -> str:
     if str.isdigit(player) and int(player) < 10: player =  " " + player
-    if player == 'X': player = f" {terminal_colors.OK_BLUE}{player}{terminal_colors.END_C}"
-    elif player == 'O': player = f" {terminal_colors.FAIL}{player}{terminal_colors.END_C}"
+    if player == PLAYERS[0]: player = f" {terminal_colors.OK_BLUE}{player}{terminal_colors.END_C}"
+    elif player == PLAYERS[1]: player = f" {terminal_colors.FAIL}{player}{terminal_colors.END_C}"
     return player
--- play.py
+++ play.py
@@ -1,59 +1,77 @@
-#import os
-from helpers import draw_board, check_side, check_win, format_player, terminal_colors
+import os
+from helpers import draw_board, check_side, check_win, get_column, format_player, ROW_COUNT, COLUMN_COUNT, BOARD_SIZE, PLAYERS, terminal_colors
+import pprint
 
 class Game:
     def __init__(self) -> None:
-        #rows, cols = (6, 7)
-        #self.board: list[str] = ["."]*cols*rows
-        self.board: list[str] = list(map(str, range(0, 41 + 1)))
-        self.playing = True
-        # add list to track all moves
-        self.last_move = -1
-        self.won = False
+        self.board: list[str] = list(map(str, range(0, BOARD_SIZE)))
+        self.playing: bool = True
+        # List to track all moves
+        self.moves: list[int] = []
+        self.won: bool = False
         # Half moves
-        self.ply = 0
-        self.message = ""
+        self.ply: int = 0
+        self.message: str = ""
 
+    # Check if a spot is available
     def is_available(self, move: int) -> bool:
-        print(move)
-        if move > 41: return False
-        return True if not self.board[move] in {"X", "O"} else False
+        if move > BOARD_SIZE - 1: return False
+        return True if not self.board[move] in PLAYERS.values() else False
+    
+    # Get a list of all the possible moves on the current board
+    def get_available_moves(self) -> list[int]:
+        moves: list[int] = []
+        for spot in enumerate(self.board):
+            if self.is_available(spot[0]): moves.append(spot[0])
+        moves.sort()
+        return moves
 
-    def make_move(self, move: int, player: str):
-        #go down in the column until a spot occupied is found and make the move on the spot above
+    # Make a move in a given column
+    def make_move(self, move: int, player: str) -> int:
+        # Go down in the column until a spot occupied is found and make the move on the spot above
         while self.is_available(move):
-            move += 7
-        move -= 7
-        print(move, " make")
+            move += COLUMN_COUNT
+        move -= COLUMN_COUNT
         self.board[move] = player
-        self.last_move = move
+        self.moves.append(move)
+        self.won = check_win(self.board, move, player)
+        self.ply += 1
         return move
     
-    def revert_move(self, move: int):
+    # Remove the move in a given spot
+    def revert_move(self, move: int) -> int:
         self.board[move] = str(move)
+        self.won = False
+        self.ply -= 1
+        return self.moves.pop(move)
 
-    def revert_last_move(self) -> None:
-        if 0 <= self.last_move <= 41:
-            self.board[self.last_move] = str(self.last_move)
-        # When the list to track all moves is implemented pop the previous last move to store it here
-        self.last_move = -1
+    # Remove the last move made in a column
+    def revert_last_move(self, move: int) -> int:
+        while self.is_available(move):
+            move += COLUMN_COUNT
+        self.board[move] = str(move)
+        self.moves.pop()
+        self.won = False
+        self.ply -= 1
+        return move
 
+    # Print the board and end game message
     def print_end_game(self):
-        draw_board(self.board, self.last_move)
+        draw_board(self.board, self.moves.pop())
         # Print winner if applicable
         if self.won: print(f"Player '{format_player(check_side(self.ply-1))}' {terminal_colors.OK_GREEN}Wins!{terminal_colors.END_C}")
         else: print(f"{terminal_colors.OK_CYAN}{terminal_colors.BOLD}GAME OVER - Tie!{terminal_colors.END_C}")
 
+# Play game with two human players
 def human_human():
     game = Game()
-
     while game.playing:
         # Reset the screen
         player = check_side(game.ply)
         draw_board(game.board)
         print(game.message + f"\nTurn {game.ply + 1} for player {format_player(player)} or press 'q' to quit")
-        #t0e = Kibitzer()
-        #t0e.best_move(game, game.ply)
+        t0e = Kibitzer()
+        t0e.best_move(game)
         # Get input from player
         move = input()
         move_int = -1
@@ -63,43 +81,81 @@ def human_human():
             print(f"{terminal_colors.OK_CYAN}{terminal_colors.BOLD}GAME TERMINATED{terminal_colors.END_C}")
         elif str.isdigit(move):
             move_int = int(move)
-            if 0 <= move_int <= 6:
+            if 0 <= move_int <= ROW_COUNT:
                 # Check if spot is taken
                 if game.is_available(move_int):
                     # Update board
                     move_int = game.make_move(move_int, player)
-                    game.ply += 1
                     game.message = ""
-                    if check_win(game.board, move_int, player): game.playing, game.won = False, True
-                    if game.ply > 41: game.playing = False
+                    if game.won: game.playing = False
+                    if game.ply > BOARD_SIZE - 1: game.playing = False
                 else: game.message = f"{terminal_colors.WARNING}Warning: Spot already taken{terminal_colors.END_C}"
             else: game.message = f"{terminal_colors.FAIL}Invalid move{terminal_colors.END_C}"
         else: game.message = f"{terminal_colors.FAIL}Invalid input{terminal_colors.END_C}"
 
     game.print_end_game()
 
+# Play a game against the AI
+def human_AI():
+    game = Game()
+    while game.playing:
+        # Reset the screen
+        player = check_side(game.ply)
+        draw_board(game.board)
+        print(game.message + f"\nTurn {game.ply + 1} for player {format_player(player)} or press 'q' to quit")
+        if player == 'O':
+            c04 = Kibitzer()
+            move = c04.best_move(game)[1]
+            move_int = get_column(move)
+            if 0 <= move_int < len(game.board):
+                if game.is_available(move_int):
+                    # Update board
+                    game.make_move(move_int, player)
+                    game.message = ""
+        else:
+            move = input()
+            move_int = -1
+            if move == 'q':
+                game.playing = False
+                draw_board(game.board)
+                print(f"{terminal_colors.OK_CYAN}{terminal_colors.BOLD}GAME TERMINATED{terminal_colors.END_C}")
+            elif str.isdigit(move):
+                move_int = int(move)
+                if 0 <= move_int <= ROW_COUNT:
+                    # Check if spot is taken
+                    if game.is_available(move_int):
+                        # Update board
+                        move_int = game.make_move(move_int, player)
+                        game.message = ""
+                        if game.won: game.playing = False
+                        if game.ply > BOARD_SIZE - 1: game.playing = False
+                    else: game.message = f"{terminal_colors.WARNING}Warning: Spot already taken{terminal_colors.END_C}"
+                else: game.message = f"{terminal_colors.FAIL}Invalid move{terminal_colors.END_C}"
+            else: game.message = f"{terminal_colors.FAIL}Invalid input{terminal_colors.END_C}"
+        if game.won: game.playing = False
+        if game.ply > BOARD_SIZE - 1: game.playing = False
+        
+    game.print_end_game()
+
+
+
+# Simulate game with two AI players
 def AI_AI():
     game = Game()
 
     while game.playing:
         c04 = Kibitzer()
         player = check_side(game.ply)
-        move = c04.best_move(game, game.ply)[0].position
-        move_int = int(move) - 1
+        move = c04.best_move(game)[1]
+        move_int = get_column(move)
         if 0 <= move_int < len(game.board):
             if game.is_available(move_int):
                 # Update board
                 game.make_move(move_int, player)
-                game.ply += 1
                 game.message = ""
-
-        if check_win(game.board, move, player): game.playing, game.won = False, True
-        if game.ply > 8: game.playing = False
-
+        if game.won: game.playing = False
+        if game.ply > BOARD_SIZE - 1: game.playing = False
     game.print_end_game()
-
-from helpers import check_side, check_win
-import pprint
 
 class Move:
     def __init__(self, score:float, position:int, player:str) -> None:
@@ -108,66 +164,66 @@ class Move:
         self.player = player
 
     def __repr__(self):
-        return f"score: {100 - self.score}' position: {self.position}) player: {self.player}"
+        return f"score: {100 - self.score} position: {self.position} player: {self.player}"
 
 class Kibitzer:
-    def __init__(self, p1:str='O', p2:str='X') -> None:
-        self.p1 = p1
-        self.p2 = p2
+    def __init__(self) -> None:
         self.moves:dict[int, Move] = {}
         # Max values are bigger than 100 to search for the fastest win or slowest loss using depth
-        self.scores:dict[str, float] = {p1: -100, p2: 100, 'tie': 0}
+        self.scores:dict[str, float] = {PLAYERS[0]: -10000000000, PLAYERS[1]: 10000000000, 'tie': 0}
 
-    def minimax(self, game: Game, depth: int, is_maximizing: bool, ply: int)->float:
-        curr_side = check_side(ply)
-        if check_win(game.board, game.last_move, curr_side):
+    # Simple implementation of the minimax function
+    def minimax(self, game: Game, depth: int) -> tuple[float, int]:
+        curr_side = check_side(game.ply)
+        is_maximizing = False
+        if curr_side == PLAYERS[0]: is_maximizing = True
+        if game.won:
             score = self.scores[curr_side] - depth
             if is_maximizing:
                 score = self.scores[curr_side] + depth
-            return score
-        elif ply > 8: return self.scores['tie']
-
+            return score, -1
+        elif game.ply > BOARD_SIZE - 1 or  depth >= 3: return self.scores['tie'], -1
+        moves = game.get_available_moves()
         limit_eval = float('inf')
-        player = self.p2
-        if is_maximizing: 
-            limit_eval = float('-inf')
-            player = self.p1
-        for i in range(7):
+        if is_maximizing: limit_eval = float('-inf')
+        best_move = -1
+        for spot in moves:
             # Check if the spot is available
-            if not game.board[i] in {'X', 'O'}:
-                last = game.make_move(i, player)
-                eval = self.minimax(game, depth + 1, not is_maximizing, ply + 1)
-                game.revert_move(last)
-                #max_eval = max(max_eval, eval)
-                if (is_maximizing and eval > limit_eval) or ((not is_maximizing) and eval < limit_eval):
-                    limit_eval = eval
-                    self.moves[depth] = Move(eval, i+1, curr_side)
-        self.moves = dict(sorted(self.moves.items()))
-        return limit_eval
+            last_move = game.make_move(get_column(spot), curr_side)
+            eval = self.minimax(game, depth + 1)[0]
+            game.revert_last_move(spot)
+            if (is_maximizing and eval > limit_eval) or ((not is_maximizing) and eval < limit_eval):
+                limit_eval = eval
+                best_move = last_move
+        return limit_eval, best_move
 
-    def best_move(self, game: Game, ply: int):
-        self.moves = {}
-        # p1 to make its turn
-        max = False
-        if check_side(ply) == 'O': max = True
-        pprint.pprint(f"minimax: {self.minimax(game, 0, max, ply)}, best move: {self.moves}")
-        return self.moves
+    # Get the best move using the minimax function
+    def best_move(self, game: Game):
+        best_move = self.minimax(game, 0)
+        pprint.pprint(f"best move: {best_move}")
+        return best_move
 
 
 
-
+# Menu to load game mode
 def main_menu() -> None:
-    """test = Game()
-    test.board[4] = "X"
-    test.board[9] = "X"
-    test.board[16] = "X"
-    #test.board[23] = "X"
-    #test.board[3] = "X"
-    #test.board[4] = "X"
-    #test.board[5] = "X"
-    test.board[10] = "X"
-    test.board[18] = "X"
-    test.board[22] = "X"
-    draw_board(test.board)
-    print(check_win(test.board, 16, "X"))"""
-    human_human()
+    waiting = True
+    while waiting:
+        print("Welcome! Please select a game mode from the menu below or press '0' to quit")
+        print("     1. Human vs Human")
+        print("     2. Human vs AI")
+        print("     3. AI vs AI")
+        print("     0. Quit game")
+        selection = input()
+        match selection:
+            case '0':
+                waiting = False
+            case '1':
+                human_human()
+            case '2':
+                human_AI()
+            case '3':
+                AI_AI()
+            case _:
+                os.system('cls' if os.name == 'nt' else 'clear')
+                print(f"{terminal_colors.FAIL}Invalid option{terminal_colors.END_C}")
